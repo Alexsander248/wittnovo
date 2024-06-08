daabaa40579616c5ba19fdd0c3bbc57fa1676874
Alexsander248 <alexsander.assis@outlook.com>
2024-06-08 20:39:40 -0300

    mudanças

--- js/animar.js
+++
@@ -1,8 +0,0 @@
-const bntMobile = document.querySelector(".btn-menu-mobile");
-const navegacaoMob = document.querySelector(".navegacao-mobile");
-
-bntMobile.addEventListener("click", () => {
-  console.log("botao clicado");
-  navegacaoMob.classList.toggle("abrir");
-  bntMobile.classList.toggle("ativo");
-});
